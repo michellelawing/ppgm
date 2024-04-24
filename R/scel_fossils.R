d1b4197f85a32f91793cb0b5b769dc6adf6ae19d
Michelle Lawing <alawing@tamu.edu>
2024-04-24 16:56:09 -0500

    Update scel_fossils.R

--- R/scel_fossils.R
+++ R/scel_fossils.R
@@ -1,7 +1,7 @@
 #' Sceloporus fossil data
 #' 
 #' Fossil occurrences for Sceloporus, as collected for Lawing et al 2016. Occurrence records from Paleobiology Database. See reference for further details
-#' Lawing et al (2015) Including fossils in phylogenetic climate reconstructions: A deep time perspective on the climatic niche evolution and diversification of spiny lizards (Sceloporus)
+#' Lawing et al (2016) Including fossils in phylogenetic climate reconstructions: A deep time perspective on the climatic niche evolution and diversification of spiny lizards (Sceloporus)
 #' 
 #' @format 'scel_fossils'
 #' A data frame where each row is a single fossil
